8fe354de2c418ec8098203f6d61e35ff4399a888
Lee <Joseph.Lee@ec.gc.ca>
2025-07-22 11:38:59 -0400

    more bs4 knowledge

--- src/basics2.py
+++ src/basics2.py
@@ -1,7 +1,27 @@
 from bs4 import BeautifulSoup
+import re # regular expression
 
 with open("public/index2.html", 'r') as file:
     doc = BeautifulSoup(file, "html.parser")
 
-result = doc.find("option")
-print(result)
+# Changing the filtering tag
+tag = doc.find("option")
+#changing tag attributes
+tag['value'] = "newvalue"
+print(tag.attrs)
+
+# Finding multiple tags
+tag2 = doc.find_all(["p", "div", "h2"])
+tag3 = doc.find_all(["option"], text="Undergraduate", value="undergraduate")
+
+# Looking for class names
+tag4 = doc.find_all(class_="btn-item")
+tag5 = doc.find_all(text=re.compile("\$.*"))
+# print('\n'.join(tag.strip() for tag in tag5))
+
+tag6 = doc.find_all("input", type="text")
+for tag in tag6:
+    tag['placeholder'] = "NEWVALUE"
+
+with open("changed.html", "w") as file2:
+    file2.write(str(doc))
